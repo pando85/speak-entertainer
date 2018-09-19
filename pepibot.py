31d008f4bf3f0c613bff1b44b8d9373cdfa8b7da
Oscar <ohermosa@gmail.com>
2018-09-20 00:23:54 +0200

    fix errors, log activity and systemd (#4)
    * fix errors and log activity
    
    * fix README and delete prints in pepibot.py
    
    * pepibot.service for systemd

--- pepibot.py
+++ pepibot.py
@@ -5,26 +5,31 @@ import telepot
 from telepot.loop import MessageLoop
 import speak
 import yaml
-
-# leyendo variables de pepibot.yml
-stream = open("pepibot.yml","r")
-my_dict = yaml.load_all(stream)
-for key in my_dict:
-  token = key['token']
-  authorized_ids = key['authorized_ids']
-
+import syslog
 
 def handle(msg):
   content_type, chat_type, chat_id = telepot.glance(msg)
   if chat_id not in authorized_ids:
+      log = 'El usuario %s esta perdiendo el tiempo intentando que diga \"%s\"' %(telegram.getChat(chat_id)['first_name'],msg['txt'])
       return
   if content_type == 'text':
     speak.reproduce_file(speak.get_text_to_speech_file(msg['text']))
+    log = 'El usuario %s me ha obligado a decir \"%s\"' %(telegram.getChat(chat_id)['first_name'],msg['txt'])
+  syslog.syslog(log)
 
-    
-if name == "main":
+
+if __name__ == "__main__":
+  # leyendo variables de pepibot.yml
+  stream = open("pepibot.yml","r")
+  my_dict = yaml.load_all(stream)
+  for key in my_dict:
+    token = key['token']
+    authorized_ids = key['authorized_ids']
+
   telegram = telepot.Bot(token)
 
   MessageLoop(telegram,handle).run_as_thread()
   while 1:
     time.sleep(1)
+
+    
